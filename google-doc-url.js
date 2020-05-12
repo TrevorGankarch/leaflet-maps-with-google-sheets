be4acada7669e71f5ebbd6a7345301ec67b59c75
Trevor Gankarch <65071457+TrevorGankarch@users.noreply.github.com>
2020-05-12 10:32:32 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SBcurr4turOA5fP-nn_LjSQecf_Ctf5qsrhpD958zis/edit#gid=0';
